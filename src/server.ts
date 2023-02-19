a7732ecab84fa8a3fb03d323c9109a8d219c3b9b
deividhy <deividhytonetti@hotmail.com>
2023-02-19 16:34:40 -0300

    [Fix]: ALterando modo de build para vercel

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,4 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000 '))
+app.listen(3000 || process.env.PORT, () => console.log('Servidor rodando na porta 3000 '))
